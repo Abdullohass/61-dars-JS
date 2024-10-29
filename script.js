c2057c4cec02eb57dfe2c8121964c35241b78faa
Abdullohass <abdulloh.s.gk@gmail.com>
2024-10-29 16:01:48 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// // localStorage.setItem("parol","123456789"); 
+// let p=localStorage.getItem("parol")
+// console.log(p);
+// localStorage.removeItem("parol")
+// localStorage.clear()
+
+
+// sessionStorage.setItem("ism","Abdulloh")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
